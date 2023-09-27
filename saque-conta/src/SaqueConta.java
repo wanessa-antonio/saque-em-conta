f077c8823fd14702faf4054e1e79172107d5d2a8
Wanessa <wanessa.ant04@gmail.com>
2023-09-27 18:44:02 -0300

    commit add.

--- saque-conta/src/SaqueConta.java
+++ saque-conta/src/SaqueConta.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
-public class SaqueConta {
-    
-    public static void main(String[] args) {
 
-        try (// Entrada de dados
-        Scanner scanner = new Scanner(System.in)) {
+public class SaqueConta {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            // Solicita ao usuário que insira o valor do saldo atual
+            System.out.print("Digite o valor do saldo atual: ");
             int saldoTotal = scanner.nextInt();
+
+            // Solicita ao usuário que insira o valor do saque
+            System.out.print("Digite o valor do saque: ");
             int valorSaque = scanner.nextInt();
 
             // Verificar se o saldo é suficiente para o saque
@@ -13,7 +17,7 @@ public class SaqueConta {
                 saldoTotal -= valorSaque;
                 System.out.println("Saque realizado com sucesso. Novo saldo: " + saldoTotal);
             } else {
-                System.out.println("Saldo insuficiente. Saque nao realizado!");
+                System.out.println("Saldo insuficiente. Saque não realizado!");
             }
         }
     }
